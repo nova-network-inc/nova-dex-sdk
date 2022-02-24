9a871937013762439af6db32f65f8761eedd37b8
Paulo Baronceli <paulo@baronceli.com>
2022-02-25 08:46:53 +1300

    Update token.ts

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -80,5 +80,5 @@ export const WETH = {
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.NOVA]: new Token(ChainId.NOVA, '0x657a66332A65B535Da6C5d67b8cD1D410c161a08', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.NOVA]: new Token(ChainId.NOVA, '0x657a66332A65B535Da6C5d67b8cD1D410c161a08', 18, 'WSNT', 'Wrapped SNT')
 }
